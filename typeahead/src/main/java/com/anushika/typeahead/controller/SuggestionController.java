9e9ad6faea86994ae6e9965509029fbb679e7839
Anushika/06 <anushika.24bcs10344@sst.scaler.com>
2026-06-20 18:12:22 +0530

    feat: connect frontend and backend

--- typeahead/src/main/java/com/anushika/typeahead/controller/SuggestionController.java
+++ typeahead/src/main/java/com/anushika/typeahead/controller/SuggestionController.java
@@ -6,9 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class SuggestionController {
 
